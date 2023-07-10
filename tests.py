6c8d6b340b115a722dd7d15c6cc4515c7b53068a
JanisChiffelle <janischiffelle@hotmail.ch>
2023-07-10 11:20:06 +0200

    add tests

--- tests.py
+++ tests.py
@@ -2,7 +2,41 @@ import pytest
 import os
 
 from google.cloud import storage
-from main import create_bucket
+from main import create_bucket, get_data, UrlException, HeadersException
+
+class TestIngestion:
+    url1 = "https://api.opentransportdata.swiss/TDP/Soap_Datex2/Pull"
+    url2 = "https://api.opentransportdata.swiss/TDP/Soap_Datex2/asdPull"
+    url3 = "https://api.opentransportdata.swiss/TDP/Soap_Datex2/PullThisIsDumb"
+
+    headers1 = {
+        'Content-Type': 'text/xml; charset=utf-8',
+        'Authorization': os.environ.get("OPENTRANSPORT_CREDENTIAL"),
+        'SOAPAction': 'http://opentransportdata.swiss/TDP/Soap_Datex2/Pull/v1/pullMeasurementSiteTable' 
+    }
+
+    headers2 = {
+        'Content-Type': 'text/xml; charset=utf-8',
+        'Authorization': os.environ.get("OPENTRANSPORT_CREDENTIAL") + "sdasda",
+        'SOAPAction': 'http://opentransportdata.swiss/TDP/Soap_Datex2/Pull/v1/pullMeasurementSiteTable' 
+    }
+
+    def test_get_data_url_error(self):
+        with pytest.raises(UrlException):
+            get_data(self.url2, self.headers1)
+
+    def test_get_data_correct(self):
+        assert get_data(self.url1, self.headers1) != ""
+
+    def test_get_data_after_url_correct(self):
+        try:
+            get_data(self.url3, self.headers1)
+        except UrlException:
+            assert False
+
+    def test_get_data_headers_error(self):
+        with pytest.raises(HeadersException):
+            get_data(self.url1, self.headers2) 
 
 class TestBucket:
     bucket_name = "prediswiss_test_bucket"
